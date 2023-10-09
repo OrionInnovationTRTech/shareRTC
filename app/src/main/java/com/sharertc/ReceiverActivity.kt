5de485042f5371a3145b6ad80e3ba1b85028f557
muhammedalilevent <technologiesssw@gmail.com>
2023-10-09 12:02:49 +0300

    qr scanner functionality added.

--- app/src/main/java/com/sharertc/ReceiverActivity.kt
+++ app/src/main/java/com/sharertc/ReceiverActivity.kt
@@ -1,16 +1,86 @@
 package com.sharertc
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 import com.sharertc.databinding.ActivityReceiverBinding
 
 
 class ReceiverActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityReceiverBinding
+
+    private val REQUEST_CODE_PERMISSIONS = 101
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReceiverBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (allPermissionsGranted()) {
+            startQRScanner()
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
     }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startQRScanner() {
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("QR kodunu tarayın")
+        integrator.setCameraId(0)
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+        integrator.initiateScan()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startQRScanner()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Kamera izni verilmedi, QR kod okuma işlemi yapılamaz.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result: IntentResult? = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "QR kod bulunamadı", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "QR Kod: ${result.contents}", Toast.LENGTH_SHORT).show()
+                var qrResult = result.contents
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
 }
